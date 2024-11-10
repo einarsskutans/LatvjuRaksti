e9b2c56593e18127de86e1db67e142ed99becb0c
Einars <einarsskutans@gmail.com>
2024-11-10 12:03:44 +0200

    filters

--- main.js
+++ main.js
@@ -1,64 +1,99 @@
 
 const symbolList = [
-    ["Pasaules audums", "Tīklveida režģis simbolizē pasaules vienotību, sadarbību, salīdzinot to ar bišu šūnām (1, 15-16).", "pasaulesaudums.svg"],
-    ["Dievs", "Simbolizē aizsardzību, svētību, uzraudzību, aizstāvību. Tiek salīdzināts ar jumtu (1, 21).", "dievs.svg"],
-    ["Taisnais krusts", "Nāves vai kristietības simbols. Simbolizē īslaicīgo nomiršanu un nekustīgo, arī kosmosu un pasaules centru (1, 24).", "taisnaiskrusts.svg"],
-    ["Slīpais krusts", "Simbolizē kustīgo, atbrīvoto enerģiju, arī kosmosu un pasaules centru (1, 24).", "slipaiskrusts.svg"],
-    ["Apcirptais krusts", "Simbolizē apturētu kustību, beigas (1, 135).", "apcirptaiskrusts.svg"],
-    ["Austras koks", "Saknes simbolizē pagātni - pazemi, stumbrs tagadni - cilvēkus, lapotne nākotni - debesis (1, 30).", "austraskoks.svg"],
-    ["Māras zīme", "Attēlo sievišķo, pretējo Dievam (1, 34).", "maraszime.svg"],
-    ["Taisnais Māras krusts", "Simbolizē dzīves noslēgšanu, lieto bēru cimdos (1, 35).", "taisnaismaraskrusts.svg"],
-    ["Slīpais Māras krusts", "Simbolizē dzīves dinamiskumu, dzīvības uzplaukumu (1, 35).", "slipaismaraskrusts.svg"],
-    ["Māras līklocis", "Simbolizē cikliskumu, dzīvību un nāvi (1, 35).", "marasliklocis.svg"],
-    ["Māras vārti", "Simbolizē reinkarnāciju (1, 36).", "marasvarti.svg"],
-    ["Māras zeme", "Simbolizē mieru, stabilitāti (1, 36). Arī robežu starp pasaulēm (1, 135).", "maraszeme.svg"],
-    ["Meandra cilpa", "Ūdens māte, simbolizē straumi, cikliskumu (1, 36).", "meandracilpa.svg"],
-    ["Akas zīme", "Kā četrvienība simbolizē stabilitāti, pilnību, veselumu, četrus gadalaikus, debespuses un pamatelementus (1, 40).", "akaszime.svg"],
-    ["Auseklis", "Simbolizē Gaismas koku un gaismas uzvaru cīņā pret tumsu (1, 47).", "auseklis.svg"],
-    ["Ugunskrusts", "Apzīmē aizsardzību, enerģiju, gribu, mērķtiecību. Simbolizē kustību (1, 54-55).", "ugunskrusts.svg"],
-    ["Saules zīme", "Simbolizē veselumu, mūžību, drošību, kustību. To saista ar harmoniju un dzīvību. Tā apzīmē gada galvenos notikumus - Lieldienas, Ziemassvētkus u.c. (1, 59-60).", "saule.svg"],
-    ["Jumis", "Divdabīgs, simbolizē labklājību un auglību, bet arī nāvi un pabeigtību. Saistīta ar cikla posmiem un enerģiju (1, 65-66).", "jumis.svg"],
-    ["Ūsiņš", "Spīdēt, aust. Simbolizē pavasari un gaismas atnākšanu (1, 70).", "usins.svg"],
-    ["Mēness krusts", "Simbolizē dzīvību un iznīcību. Saistīta ar zemkopību. Krusts simbolizē četras Mēness fāzes (1, 74-75).", "menesskrusts.svg"],
-    ["Pilna Mēness zīme", "Simbolizē dzīvību un iznīcību. Saistīta ar zemkopību. Nozīmē veselumu (1, 74-75).", "pilnsmeness.svg"],
-    ["Laimas krusts", "Simbolizē likteni un svētību. Krusts attēlo Dievu centrā un pasaules spēkus tās atzarojumos (1, 81).", "laimaskrusts.svg"],
-    ["Laimas skujiņa", "Skujiņa raksturo cilvēka mūžu un laika plūdumu (1, 80).", "laimasskujina.svg"],
-    ["Laimas slotiņa", "Slotiņa simbolizē laika plūsmu. Tā vidus nozīmē tagadni. Slotiņa attēlo slieksni (1, 80).", "laimasslotina.svg"],
-    ["Laimas grābeklītis", "Grabeklītis raksturo nevēlamā attīrīšanu un vēlamā piesaistīšanu (1, 122).", "laimasgrabeklitis.svg"],
-    ["Laipa", "Simbolizē līdzsvaru (1, 80).", "laipa.svg"],
-    ["Žagariņš", "Simbolizē nepabeigtību, turpinājumu un ceļu (1, 81).", "zagarins.svg"],
-    ["Zalkša zīme", "Simbolizē izmaiņas un plūsmu, līdzīgi zalkša kustībai. Raksturo līdzsvaru (1, 86-87).", "zalktis.svg"],
-    ["Spārnotais pūķis", "Simbolizē gan postošu stihisku spēku, gan atjaunotību un dzīvības enerģiju. Izpaužas kā pūķis (1, 87).", "sparnotaispukis.svg"],
-    ["Krusta zalktis", "Krusts raksturo dinamiskumu un kustību (1, 87).", "krustazalktis.svg"],
-    ["Jāņa zīme", "Attēlo 3 Saules, ko raksturo vasaras saulgrieži. Tās simbolizē ceļu, kas sākas Ziemassvētkos, gaismas kulmināciju - Jāņus un rudeni - tumsu (1, 92).", "janis.svg"],
-    ["Krupītis", "Simbolizē zemapziņu un intuīciju. Saistās ar neredzamo pasauli (1, 95).", "krupitis.svg"],
-    ["Mārtiņa zīme", "Simbolizē pārticību. Sevī ietver simetriju un dzīvības noslēpumu (1, 99).", "martinazime.svg"],
-    ["Zvaigznes zīme", "Simbolizē aizsardzību. Kristietībā - tikumība, bruņnieciskums, pieklājība, Dieva godāšana (1, 103).", "zvaigzneszime.svg"],
-    ["Punkts", "Raksturo sākumu un beigas. Salīdzināma ar zvaigzni. Simbolizē radošo spēku (1, 109).", "punkts.svg"],
-    ["Astoņnītis", "Simbolizē bezgalību, laika ciklus (1, 112).", "astonitis.svg"],
-    ["Dievaine", "Simbolizē robežsituāciju starp pasaulēm (1, 116).", "dievaine.svg"],
-    ["Kāpnes", "Savieno elli, cilvēku pasauli un debesis. Simbolizē cilvēka mūžu un dvēseles ceļu (1, 117).", "kapnes.svg"],
-    ["Apgrieztais Jumis", "Simbolizē nāvi, postošo spēku. Pretējs Jumim (1, 134).", "apgrieztaisjumis.svg"],
-    ["Taisnais Māras krusts ar atdzimšanas iespēju", "Simbolizē ciklisku maiņu un reinkarnācijas iespējamību. Raksturo nāvi un dzīvi, gaismu un tumsu (1, 136).", "taisnaismaraskrustsaratzimsanasiespeju.svg"]
+    ["Pasaules audums", "Tīklveida režģis simbolizē pasaules vienotību, sadarbību, salīdzinot to ar bišu šūnām (1, 15-16).", "pasaulesaudums.svg", 1],
+    ["Dievs", "Simbolizē aizsardzību, svētību, uzraudzību, aizstāvību. Tiek salīdzināts ar jumtu (1, 21).", "dievs.svg", 1],
+    ["Taisnais krusts", "Nāves vai kristietības simbols. Simbolizē īslaicīgo nomiršanu un nekustīgo, arī kosmosu un pasaules centru (1, 24).", "taisnaiskrusts.svg", 1],
+    ["Slīpais krusts", "Simbolizē kustīgo, atbrīvoto enerģiju, arī kosmosu un pasaules centru (1, 24).", "slipaiskrusts.svg", 1],
+    ["Apcirptais krusts", "Simbolizē apturētu kustību, beigas (1, 135).", "apcirptaiskrusts.svg", 1],
+    ["Austras koks", "Saknes simbolizē pagātni - pazemi, stumbrs tagadni - cilvēkus, lapotne nākotni - debesis (1, 30).", "austraskoks.svg", 1],
+    ["Māras zīme", "Attēlo sievišķo, pretējo Dievam (1, 34).", "maraszime.svg", 2],
+    ["Taisnais Māras krusts", "Simbolizē dzīves noslēgšanu, lieto bēru cimdos (1, 35).", "taisnaismaraskrusts.svg", 1],
+    ["Slīpais Māras krusts", "Simbolizē dzīves dinamiskumu, dzīvības uzplaukumu (1, 35).", "slipaismaraskrusts.svg", 1],
+    ["Māras līklocis", "Simbolizē cikliskumu, dzīvību un nāvi (1, 35).", "marasliklocis.svg", 1],
+    ["Māras vārti", "Simbolizē reinkarnāciju (1, 36).", "marasvarti.svg", 1],
+    ["Māras zeme", "Simbolizē mieru, stabilitāti (1, 36). Arī robežu starp pasaulēm (1, 135).", "maraszeme.svg", 1],
+    ["Meandra cilpa", "Ūdens māte, simbolizē straumi, cikliskumu (1, 36).", "meandracilpa.svg", 1],
+    ["Akas zīme", "Kā četrvienība simbolizē stabilitāti, pilnību, veselumu, četrus gadalaikus, debespuses un pamatelementus (1, 40).", "akaszime.svg", 1],
+    ["Auseklis", "Simbolizē Gaismas koku un gaismas uzvaru cīņā pret tumsu (1, 47).", "auseklis.svg", 1],
+    ["Ugunskrusts", "Apzīmē aizsardzību, enerģiju, gribu, mērķtiecību. Simbolizē kustību (1, 54-55).", "ugunskrusts.svg", 1],
+    ["Saules zīme", "Simbolizē veselumu, mūžību, drošību, kustību. To saista ar harmoniju un dzīvību. Tā apzīmē gada galvenos notikumus - Lieldienas, Ziemassvētkus u.c. (1, 59-60).", "saule.svg", 1],
+    ["Jumis", "Divdabīgs, simbolizē labklājību un auglību, bet arī nāvi un pabeigtību. Saistīta ar cikla posmiem un enerģiju (1, 65-66).", "jumis.svg", 1],
+    ["Ūsiņš", "Spīdēt, aust. Simbolizē pavasari un gaismas atnākšanu (1, 70).", "usins.svg", 1],
+    ["Mēness krusts", "Simbolizē dzīvību un iznīcību. Saistīta ar zemkopību. Krusts simbolizē četras Mēness fāzes (1, 74-75).", "menesskrusts.svg", 1],
+    ["Pilna Mēness zīme", "Simbolizē dzīvību un iznīcību. Saistīta ar zemkopību. Nozīmē veselumu (1, 74-75).", "pilnsmeness.svg", 1],
+    ["Laimas krusts", "Simbolizē likteni un svētību. Krusts attēlo Dievu centrā un pasaules spēkus tās atzarojumos (1, 81).", "laimaskrusts.svg", 2],
+    ["Laimas skujiņa", "Skujiņa raksturo cilvēka mūžu un laika plūdumu (1, 80).", "laimasskujina.svg", 2],
+    ["Laimas slotiņa", "Slotiņa simbolizē laika plūsmu. Tā vidus nozīmē tagadni. Slotiņa attēlo slieksni (1, 80).", "laimasslotina.svg", 2],
+    ["Laimas grābeklītis", "Grabeklītis raksturo nevēlamā attīrīšanu un vēlamā piesaistīšanu (1, 122).", "laimasgrabeklitis.svg", 1],
+    ["Laipa", "Simbolizē līdzsvaru (1, 80).", "laipa.svg", 1],
+    ["Žagariņš", "Simbolizē nepabeigtību, turpinājumu un ceļu (1, 81).", "zagarins.svg", 1],
+    ["Zalkša zīme", "Simbolizē izmaiņas un plūsmu, līdzīgi zalkša kustībai. Raksturo līdzsvaru (1, 86-87).", "zalktis.svg", 1],
+    ["Spārnotais pūķis", "Simbolizē gan postošu stihisku spēku, gan atjaunotību un dzīvības enerģiju. Izpaužas kā pūķis (1, 87).", "sparnotaispukis.svg", 1],
+    ["Krusta zalktis", "Krusts raksturo dinamiskumu un kustību (1, 87).", "krustazalktis.svg", 1],
+    ["Jāņa zīme", "Attēlo 3 Saules, ko raksturo vasaras saulgrieži. Tās simbolizē ceļu, kas sākas Ziemassvētkos, gaismas kulmināciju - Jāņus un rudeni - tumsu (1, 92).", "janis.svg", 1],
+    ["Krupītis", "Simbolizē zemapziņu un intuīciju. Saistās ar neredzamo pasauli (1, 95).", "krupitis.svg", 1],
+    ["Mārtiņa zīme", "Simbolizē pārticību. Sevī ietver simetriju un dzīvības noslēpumu (1, 99).", "martinazime.svg", 1],
+    ["Zvaigznes zīme", "Simbolizē aizsardzību. Kristietībā - tikumība, bruņnieciskums, pieklājība, Dieva godāšana (1, 103).", "zvaigzneszime.svg", 1],
+    ["Punkts", "Raksturo sākumu un beigas. Salīdzināma ar zvaigzni. Simbolizē radošo spēku (1, 109).", "punkts.svg", 1],
+    ["Astoņnītis", "Simbolizē bezgalību, laika ciklus (1, 112).", "astonitis.svg", 1],
+    ["Dievaine", "Simbolizē robežsituāciju starp pasaulēm (1, 116).", "dievaine.svg", 0],
+    ["Kāpnes", "Savieno elli, cilvēku pasauli un debesis. Simbolizē cilvēka mūžu un dvēseles ceļu (1, 117).", "kapnes.svg", 0],
+    ["Apgrieztais Jumis", "Simbolizē nāvi, postošo spēku. Pretējs Jumim (1, 134).", "apgrieztaisjumis.svg", 3],
+    ["Taisnais Māras krusts ar atdzimšanas iespēju", "Simbolizē ciklisku maiņu un reinkarnācijas iespējamību. Raksturo nāvi un dzīvi, gaismu un tumsu (1, 136).", "taisnaismaraskrustsaratzimsanasiespeju.svg", 3]
 ]
 
-symbolsGenerated = false
+let symbolsGenerated = false
+let symbolsSelected = -1; // Default, load all symbols
 
-function insertCard() {
+let symbolCategoryList = [[], [], [], []]
+
+function initializeSymbols() {
+    symbolCategoryList = [[], [], [], []]
+    for (list of symbolList) {
+        symbolCategoryList[list[3]].push(list)
+    }
+    console.log(symbolCategoryList)
+    generateSymbols()
+}
+
+function generateSymbols() {
+    let divtable = document.getElementsByClassName("divtable")[0]
+    divtable.innerHTML = ''
+
+    switch (symbolsSelected) {
+        case -1:
+            divtable.innerHTML += '<section class="category"><div class="h3bar"><h3>Visas zīmes</h3></div><div class="categorylist"></div></section>'
+            break
+        case 0:
+            divtable.innerHTML += '<section class="category"><div class="h3bar"><h3>Debešu zīmes</h3></div><div class="categorylist"></div></section>'
+            break
+        case 1:
+            divtable.innerHTML += '<section class="category"><div class="h3bar"><h3>Gadskārtu zīmes</h3></div><div class="categorylist"></div></section>'
+            break
+        case 2:
+            divtable.innerHTML += '<section class="category"><div class="h3bar"><h3>Dievību zīmes</h3></div><div class="categorylist"></div></section>'
+            break
+        case 3:
+            divtable.innerHTML += '<section class="category"><div class="h3bar"><h3>Citas zīmes</h3></div><div class="categorylist"></div></section>'
+            break
+        default:
+            break
+    }
     let parent = document.getElementsByClassName("categorylist")[0]
-
-    if (!symbolsGenerated) {
+    if (symbolsSelected !== -1) {
+        for (list of symbolCategoryList[symbolsSelected]) {
+            parent.innerHTML += '<div class="cardzime"><img src="LatvjuZimesAssets/'+ list[2] +'"><p>' + list[0] + '</p></div>'
+        } 
+    }
+    if (symbolsSelected === -1) {
         for (list of symbolList) {
-            console.log(list[0])
             parent.innerHTML += '<div class="cardzime"><img src="LatvjuZimesAssets/'+ list[2] +'"><p>' + list[0] + '</p></div>'
         }
     }
-    symbolsGenerated = true
+}
+function setSelection(symbol) {
+    symbolsSelected = symbol
+    generateSymbols()
 }
 
-function insertCardDescription() {
-    let parent = document.getElementsByClassName("cardzime")
-    parent.innerHTML += '<h1>TEST</h1>'
-}
-
-document.addEventListener("DOMContentLoaded", insertCard)
+document.addEventListener("DOMContentLoaded", initializeSymbols)
